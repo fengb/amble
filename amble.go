af0a7a33fbd7ba927c71d9845033f0afbb1be1aa
Benjamin Feng <contact@fengb.me>
2017-03-29 23:36:36 -0500

    Pipe in headers

--- amble.go
+++ amble.go
@@ -51,8 +51,8 @@ func stream(url string, headers map[string]string) error {
 		return err
 	}
 
-	for key, value := range headers {
-		req.Header.Set(key, value)
+	for k, v := range headers {
+		req.Header.Set(k, v)
 	}
 
 	resp, err := client.Do(req)
@@ -64,12 +64,30 @@ func stream(url string, headers map[string]string) error {
 	if err != nil {
 		return err
 	}
-	fmt.Printf("%s - %s\n", url, resp.Status)
+	fmt.Println(url, resp.Status)
 	resp.Header.Write(os.Stdout)
 	fmt.Println(string(body))
 	return nil
 }
 
+func isInteractive(f *os.File) bool {
+	info, err := f.Stat()
+	if err != nil {
+		return false
+	}
+
+	return (info.Mode() & os.ModeCharDevice) == os.ModeCharDevice
+}
+
 func main() {
-	stream("http://google.com/", make(map[string]string))
+	headers := make(map[string]string)
+	if !isInteractive(os.Stdin) {
+		pipeHeaders, _ := ParseHeaders(os.Stdin)
+		if pipeHeaders != nil {
+			for k, v := range pipeHeaders {
+				headers[k] = v
+			}
+		}
+	}
+	stream("http://google.com/", headers)
 }
